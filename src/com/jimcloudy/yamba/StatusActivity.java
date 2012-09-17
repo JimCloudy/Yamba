1dd5356c28120a69f93a24ee00c4859bc45bde0a
JimCloudy <lynchbrent@hotmail.com>
2012-09-17 14:51:41 -0500

    up to page 13 ... again

--- src/com/jimcloudy/yamba/StatusActivity.java
+++ src/com/jimcloudy/yamba/StatusActivity.java
@@ -1,20 +1,124 @@
 package com.jimcloudy.yamba;
 
-import android.os.Bundle;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StatusActivity extends Activity {
-
+public class StatusActivity extends Activity implements OnClickListener,TextWatcher{
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button buttonUpdate;
+	TextView textCount;
+	SharedPreferences prefs;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        
+        //find views
+        editText = (EditText) findViewById(R.id.editText);
+        buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
+        buttonUpdate.setOnClickListener(this);
+        
+        textCount = (TextView) findViewById(R.id.textCount);
+        textCount.setText(Integer.toString(140));
+        textCount.setTextColor(Color.GREEN);
+        
+        editText.addTextChangedListener(this);
+        editText.setBackgroundColor(5);
+        }
+    
+    public void onClick(View v){
+    	String status = editText.getText().toString();
+    	new PostToTwitter().execute(status);
+    	editText.setText("");
+    	Log.d(TAG, "onClicked");
     }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_status, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch(item.getItemId()){
+    	case R.id.itemPrefs:
+    		startActivity(new Intent(this,PrefsActivity.class));
+    		break;
+    	case R.id.itemServiceStart:
+    		startService(new Intent(this,UpdaterService.class));
+    		break;
+    	case R.id.itemServiceStop:
+    		stopService(new Intent(this,UpdaterService.class));
+    		break;
+    	}
+    	return true;
+    }
+
+    class PostToTwitter extends AsyncTask<String, Integer, String>{
+    	@Override
+    	protected String doInBackground(String...statuses){
+    		YambaApplication yamba = ((YambaApplication)getApplication());
+    		try{
+    			yamba.getTwitter();
+    			yamba.twitter.updateStatus(statuses[0]);
+    			return "Posted";
+    		}
+    		catch(TwitterException e){
+    			Log.e(TAG, e.toString());
+    			e.printStackTrace();
+    			return "Failed to post";
+    		}
+    	}
+    	
+    	@Override
+    	protected void onProgressUpdate(Integer...values){
+    		super.onProgressUpdate(values);
+    	}
+    	
+    	@Override
+    	protected void onPostExecute(String result){
+    		Toast.makeText(StatusActivity.this,result,Toast.LENGTH_LONG).show();
+    	}
+    }
+    
+    public void afterTextChanged(Editable statusText){
+    	int count = 140 - statusText.length();
+    	textCount.setText(Integer.toString(count));
+    	textCount.setTextColor(Color.GREEN);
+    	if(count<10)
+    	{
+    		textCount.setTextColor(Color.YELLOW);
+    	}
+    	if(count<0)
+    	{
+    		textCount.setTextColor(Color.RED);
+    	}
+    }
+    
+    public void beforeTextChanged(CharSequence s, int start, int count, int after){
+    }
+    
+    public void onTextChanged(CharSequence s, int start, int before, int count){
+    }    
 }
+
+
